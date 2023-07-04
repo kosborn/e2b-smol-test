3a0fa86f18a51d7568ef0fd356097a5681974f4f
kosborn <723326+kosborn@users.noreply.github.com>
2023-07-04 13:55:12 -0600

    configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,15 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-auto';
 import pkg from "./package.json";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    target: "#svelte",
-    vite: {
-      ssr: {
-        noExternal: Object.keys(pkg.dependencies || {}),
-      },
-      optimizeDeps: {
-        include: ["cookie", "jsonwebtoken"],
-      },
-    },
-  },
-  preprocess: preprocess(),
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
